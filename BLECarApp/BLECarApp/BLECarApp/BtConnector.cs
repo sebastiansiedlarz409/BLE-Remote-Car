e58bc5f3a3aee537769af61b40db396d92da7e22
Sebastian Siedlarz <siedlarzsebastian409@gmail.com>
2022-03-13 18:09:27 +0100

    Init APP

---
+++ BLECarApp/BLECarApp/BLECarApp/BtConnector.cs
@@ -0,0 +1,97 @@
+﻿using Plugin.BluetoothLE;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace BLECarApp
+{
+    public class BtConnector
+    {
+        private IDevice car = null;
+        private IGattCharacteristic cmdCharacteristic;
+        private IGattCharacteristic statusCharacteristic;
+        private string carName = "BTLE-REMOTE-CAR";
+
+        public BtConnector()
+        {
+
+        }
+
+        public void Connect(Action<string> action)
+        {
+            Debug.WriteLine("Scanning...");
+            try
+            {
+                CrossBleAdapter.Current.ScanForUniqueDevices().Subscribe(result =>
+                {
+                    if (result.Name == null)
+                        return;
+
+                    if (result.Name.Equals(carName))
+                    {
+                        Debug.WriteLine($"Car found {result.Name}");
+                        car = result;
+                        action("Found!");
+
+                        car.Connect();
+                        action("Connected!");
+
+                        car.WhenAnyCharacteristicDiscovered().Subscribe(chr =>
+                        {
+                            if (chr.CanWrite())
+                            {
+                                cmdCharacteristic = chr;
+                                Debug.WriteLine("Cmd characteristic found");
+                            }
+                            else if (chr.CanRead())
+                            {
+                                statusCharacteristic = chr;
+                                Debug.WriteLine("Status characteristic found");
+                            }
+                            //action("Ready!");
+                        });
+                    }
+                    else
+                    {
+                        Debug.WriteLine($"Device found {result.Name}");
+                    }
+                });
+            }
+            catch (ArgumentException)
+            {
+
+            }
+        }
+
+        public byte[] CalculateChecksum(byte[] data)
+        {
+            byte a = 0;
+            byte b = 0;
+
+            foreach (byte item in data)
+            {
+                a = (byte)((a + item) % 255);
+                b = (byte)((a + b) % 255);
+            }
+
+            return new byte[] { b, a };
+        }
+
+        public void Send(byte[] data, int size)
+        {
+            byte[] crc = CalculateChecksum(data);
+
+            data = data.ToList().Concat(crc.ToList()).ToArray();
+
+            if (cmdCharacteristic != null && car.IsConnected())
+            {
+                cmdCharacteristic.Write(data).Subscribe(result =>
+                {
+                    Debug.WriteLine($"Send {size} bytes");
+                });
+            }
+        }
+    }
+}
---
+++ BLECarApp/BLECarApp/BLECarApp/ManualView.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BLECarApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ManualView : ContentPage
+    {
+        private BtConnector bt;
+        public ManualView()
+        {
+            InitializeComponent();
+
+            ConnectionBtn.Clicked += ConnectBtn_Clicked;
+            SendDataBtn.Clicked += SendDataBtn_Clicked;
+
+            SpeedUpBtn.Clicked += SpeedUpBtn_Clicked;
+            SpeedDwBtn.Clicked += SpeedDwBtn_Clicked;
+            StopBtn.Clicked += StopBtn_Clicked;
+            LeftBtn.Clicked += LeftBtn_Clicked;
+            RightBtn.Clicked += RightBtn_Clicked;
+        }
+
+        private void RightBtn_Clicked(object sender, EventArgs e)
+        {
+            bt.Send(new byte[] { 0xEE, 0xEE, 0xEE }, 3);
+        }
+
+        private void LeftBtn_Clicked(object sender, EventArgs e)
+        {
+            bt.Send(new byte[] { 0xDD, 0xDD, 0xDD }, 3);
+        }
+
+        private void StopBtn_Clicked(object sender, EventArgs e)
+        {
+            bt.Send(new byte[] { 0xCC, 0xCC, 0xCC }, 3);
+        }
+
+        private void SpeedDwBtn_Clicked(object sender, EventArgs e)
+        {
+            bt.Send(new byte[] { 0xBB, 0xBB, 0xBB }, 3);
+        }
+
+        private void SpeedUpBtn_Clicked(object sender, EventArgs e)
+        {
+            bt.Send(new byte[] { 0xAA, 0xAA, 0xAA }, 3);
+        }
+
+        private void OnConnection(string text)
+        {
+            ConnectionBtn.Text = text;
+        }
+
+        private void ConnectBtn_Clicked(object sender, EventArgs e)
+        {
+            if (bt == null)
+                bt = new BtConnector();
+
+            bt.Connect(OnConnection);
+        }
+
+        private void SendDataBtn_Clicked(object sender, EventArgs e)
+        {
+            bt.Send(new byte[] { 0xAA, 0xAA, 0xAA }, 3);
+        }
+    }
+}
