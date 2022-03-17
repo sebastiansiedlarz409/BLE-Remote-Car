1cb4ec4752add3af780007b16bafdfc272eb1e20
Sebastian Siedlarz <siedlarzsebastian409@gmail.com>
2022-03-17 14:45:19 +0100

    Add app icon

--- BLECarApp/BLECarApp/BLECarApp.Android/MainActivity.cs
+++ BLECarApp/BLECarApp/BLECarApp.Android/MainActivity.cs
@@ -8,7 +8,7 @@ using Android.Views;
 
 namespace BLECarApp.Droid
 {
-    [Activity(Label = "BLECarApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "BLECarApp", Icon = "@mipmap/sportcar", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
--- BLECarApp/BLECarApp/BLECarApp.Android/Resources/Resource.designer.cs
+++ BLECarApp/BLECarApp/BLECarApp.Android/Resources/Resource.designer.cs
@@ -18810,6 +18810,9 @@ namespace BLECarApp.Droid
 			// aapt resource value: 0x7F0C0002
 			public const int launcher_foreground = 2131492866;
 			
+			// aapt resource value: 0x7F0C0003
+			public const int sportcar = 2131492867;
+			
 			static Mipmap()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
